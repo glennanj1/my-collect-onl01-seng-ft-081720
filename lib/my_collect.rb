96b35c42c61ecc0690e66a02acafbc13b7fd3f6b
John Glennan <glennan20@gmail.com>
2020-08-24 16:21:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
-
-def my_collect(collections)
-  #iterate over collections
+def my_collect(collection)
   i = 0
-  while collections < 0
-    yield(collections[i])
-    i += # i
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
   end
+  new_collection
 end
